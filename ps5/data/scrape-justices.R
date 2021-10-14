5c4872a02d14f758be3dbdc33c93a96bb1b95477
Kauila Tengan <ktengan22@amherst.edu>
2021-10-13 20:14:16 -0400

    answer 1.1

---
+++ ps5/data/scrape-justices.R
@@ -0,0 +1,21 @@
+library(tidyverse)
+library(robotstxt)
+library(rvest) 
+library(purrr) 
+library(janitor)
+library(knitr)
+
+url <- "https://en.wikipedia.org/wiki/List_of_justices_of_the_Supreme_Court_of_the_United_States"
+
+tables <- url %>% 
+  read_html() %>% 
+  html_elements("table")
+#pluck the html table
+scj <- html_table(tables[[2]], fill = TRUE) %>%  
+  #clean the names
+  janitor::clean_names() 
+#create an object for outpath
+outpath <- "/Users/tpkt/Desktop/College2021-2022/Stats231/Personal Repo/Stat-231-KT/ps5/data"
+#write the csv file pulling scj table and paste0 combines outpath with writecsv
+write_csv(scj, paste0(outpath, "/justices.csv"))
+ 
